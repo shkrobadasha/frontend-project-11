a3502077986687312e266835422baf2b99825725
Shkroba Daria <shkrobadaria@mail.ru>
2025-04-09 22:54:02 +0300

    try vercel fix2

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,30 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const webpack = require('webpack');
 
 module.exports = (env) => {
   return {
     resolve: {
       fallback: {
-        "querystring": require.resolve("querystring-es3"), 
-        "zlib": require.resolve("browserify-zlib"), 
-        "util": require.resolve("util/"), 
-        "crypto": require.resolve("crypto-browserify"), 
-        "stream": require.resolve("stream-browserify"), 
-        "path": require.resolve("path-browserify"), 
+        "querystring": require.resolve("querystring-es3"),
+        "zlib": require.resolve("browserify-zlib"),
+        "util": require.resolve("util/"),
+        "crypto": require.resolve("crypto-browserify"),
+        "stream": require.resolve("stream-browserify"),
+        "path": require.resolve("path-browserify"),
         "vm": require.resolve("vm-browserify"),
-        "assert": require.resolve("assert"), 
-        "url": require.resolve("url/"), 
-        "https": require.resolve("https-browserify"), 
-        "http": require.resolve("stream-http"), 
-        "buffer": require.resolve("buffer/"), 
+        "assert": require.resolve("assert"),
+        "url": require.resolve("url/"),
+        "https": require.resolve("https-browserify"),
+        "http": require.resolve("stream-http"),
+        "buffer": require.resolve("buffer/"),
       },
     },
     mode: env.mode ?? 'development',
     entry: path.resolve(__dirname, 'src', 'index.js'),
     stats: {
-      errorDetails: true, 
+      errorDetails: true,
     },
     output: {
       path: path.resolve(__dirname, 'build'),
@@ -32,17 +33,23 @@ module.exports = (env) => {
     },
     plugins: [
       new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'index.html') }),
+      new MiniCssExtractPlugin({  // Добавляем плагин
+        filename: '[name].[contenthash].css',
+      }),
       new webpack.ProgressPlugin(),
-      new webpack.ProvidePlugin({  
+      new webpack.ProvidePlugin({
         Buffer: ['buffer', 'Buffer'],
       }),
     ],
     module: {
       rules: [
-        { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
+        {
+          test: /\.css$/,
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'], // ИЗМЕНЕНО
+        },
         {
           test: /\.scss$/,
-          use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader', 'postcss-loader'], // ИЗМЕНЕНО
         },
       ]
     },
